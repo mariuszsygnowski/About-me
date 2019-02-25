a2fd0274a9baa8b68bea2371b45605d7886f068e
mariuszsygnowski <sygnowski.m@gmail.com>
2019-02-25 18:02:10 +0000

    heroku test

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: "source-map",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "docs")
+    path: path.resolve(__dirname, "build")
   },
   devServer: {
     proxy: {
